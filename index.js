e18c07f965064cc4fef69de93edf4a3b7da6a2fc
muradwahid <muradwahid334@gmail.com>
2023-09-14 18:25:53 +0600

    pc products route created

--- index.js
+++ index.js
@@ -27,12 +27,7 @@ function run() {
         data: allProducts,
       });
     });
-    app.get('/categories', async (req, res) => {
-      const allCategories = await categories.find({}).toArray();
-      res.status(200).json({
-        data: allCategories,
-      });
-    });
+
 
     app.listen(port, () => {
       console.log(`server is listening on ${port}`);
